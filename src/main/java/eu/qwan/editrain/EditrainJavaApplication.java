24cbbefa5ddae386c04ff991ea100d46d016695c
rob <rob@qwan.eu>
2022-09-10 14:31:57 +0200

    initial commit with springboot app

---
+++ src/main/java/eu/qwan/editrain/EditrainJavaApplication.java
@@ -0,0 +1,13 @@
+package eu.qwan.editrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditrainJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditrainJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/qwan/editrain/EditrainJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.qwan.editrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditrainJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
