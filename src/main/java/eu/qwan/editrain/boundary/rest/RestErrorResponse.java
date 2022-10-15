5584cb88592d99d15a3878a41b29c71b4f389eeb
Felipe Flores <felipe.flores_external@wartsila.com>
2022-10-16 00:40:08 +0200

    Refactor error response as a simpler record

--- src/main/java/eu/qwan/editrain/boundary/rest/RestErrorResponse.java
+++ src/main/java/eu/qwan/editrain/boundary/rest/RestErrorResponse.java
@@ -1,9 +1,4 @@
 package eu.qwan.editrain.boundary.rest;
 
-public class RestErrorResponse {
-    public final String error;
-
-    public RestErrorResponse(String error) {
-        this.error = error;
-    }
+public record RestErrorResponse(String error) {
 }
