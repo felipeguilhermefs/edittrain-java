12f19857e2ff12992953807f0cb07543223bba18
Felipe Flores <felipe.flores_external@wartsila.com>
2022-10-15 23:34:43 +0200

    Extract Mapper to rest courses

--- src/main/java/eu/qwan/editrain/boundary/rest/CourseController.java
+++ src/main/java/eu/qwan/editrain/boundary/rest/CourseController.java
@@ -16,50 +16,46 @@ import java.util.Optional;
 @RestController
 public class CourseController {
     private final CourseService courseService;
+    private final CourseDtoMapper mapper;
 
-    public CourseController(CourseService courseService) {
+    public CourseController(
+        CourseService courseService,
+        CourseDtoMapper mapper
+    ) {
         this.courseService = courseService;
+        this.mapper = mapper;
     }
 
     @GetMapping("/courses")
     public ResponseEntity<List<CourseDto>> getCourses() {
-        return ResponseEntity.ok(courseService.findAll().stream().map(this::toDto).collect(toList()));
+        return ResponseEntity.ok(
+            courseService.findAll()
+                .stream()
+                .map(mapper::toDto)
+                .collect(toList())
+        );
     }
 
     @PostMapping(value="/courses", consumes="application/json", produces="application/json")
     public ResponseEntity<Optional<CourseDto>> createCourse(@RequestBody @Valid CourseDto body) {
-        var course= courseService.create(toModel(body)).map(this::toDto);
-        return new ResponseEntity<>(course, HttpStatus.CREATED);
+        var course = mapper.toModel(body);
+        return new ResponseEntity<>(
+            courseService.create(course).map(mapper::toDto),
+            HttpStatus.CREATED
+        );
     }
 
     @PutMapping(value="/courses", consumes="application/json", produces="application/json")
     public ResponseEntity<Optional<CourseDto>> updateCourse(@RequestBody @Valid CourseDto body) {
         if (body.getId() == null || body.getId().isBlank()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-        courseService.update(toModel(body));
+        var course = mapper.toModel(body);
+        courseService.update(course);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @ExceptionHandler(value = EdiTrainException.class)
     public ResponseEntity<ErrorResponse> handleEdiTrainExceptions(EdiTrainException exception) {
         return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.BAD_REQUEST);
-    }
-
-    private CourseDto toDto(Course model) {
-        var course = new CourseDto();
-        course.setId(model.getId());
-        course.setName(model.getName());
-        course.setDescription(model.getDescription());
-        course.setTeacher(model.getTeacher());
-        return course;
-    }
-
-    public Course toModel(CourseDto dto) {
-        var course = new Course();
-        course.setId(dto.getId());
-        course.setName(dto.getName());
-        course.setDescription(dto.getDescription());
-        course.setTeacher(dto.getTeacher());
-        return course;
     }
 }
